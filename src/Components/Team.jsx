9e84938a89c381fd5732d444bef1c515737dcc8a
Jayant <jayantsingh331@gmail.com>
2021-12-26 23:40:39 +0530

    nm

--- src/Components/Team.jsx
+++ src/Components/Team.jsx
@@ -3,7 +3,25 @@ import React from 'react'
 export default function Team() {
     return (
         <>
-           <h1>Under Construction</h1> 
+           <div className="d-flex justify-content-around">
+           <div className="card_w_title">
+           <h1>Group Leader</h1>
+               <div className="grp-item d-flex flex-column align-items-center">
+         <img src={require('../images/team_pics/sachin_srivastav.png').default} alt="" />
+                <p><a href="">Sachin Kumar Srivastava</a></p>  
+                <p>Assistant Professor</p>  
+               </div>
+           </div>
+
+           <div className="card_w_title">
+           <h1>Post doc</h1>
+               <div className="grp-item d-flex flex-column align-items-center">
+         <img src={require('../images/team_pics/postdoc.png').default} alt="" />
+                <p>Yadvendra Singh</p>  
+                <p>PhD (IIT-ISM, Dhanbad)</p>  
+               </div>
+           </div>
+           </div>
         </>
     )
 }
